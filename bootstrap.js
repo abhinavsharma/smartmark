92569aed2fdeb15956d7493689613b51449ea16f
Abhinav Sharma <me@abhinavsharma.me>
2011-05-24 15:39:17 -0700

    stuff

---
+++ bootstrap.js
@@ -0,0 +1,11 @@
+function startup(data, reason) {
+  Cu.reportError("Hi!");
+}
+
+function shutdown(data, reason) {
+
+}
+
+function install (data, reason) {}
+
+function uninstall (data, reason) {}
